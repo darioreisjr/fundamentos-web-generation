ef1032aa8aec47ad1b1697c47692fced0734e4cb
Dario Reis <guttyerresjunior1995@gmail.com>
2022-07-25 18:26:51 -0300

    adiciona validação de formulario

--- assents/js/scriptContatos.js
+++ assents/js/scriptContatos.js
@@ -4,9 +4,21 @@ let email = document.getElementById('email');
 let message = document.getElementById('message');
 
 
-function enviar () {
-    if( nome.value == "" || email.value == "" || message.value == "") {
-        alert('mensagem não pode ser enviada campos precisa ser preenchido corretamente');
+//function enviar () {
+  //  if( nome.value == "" || email.value == "" || message.value == "") {
+    //    alert('mensagem não pode ser enviada campos precisa ser preenchido corretamente');
+    //} else {
+      //  alert('Mensagem enviada com sucesso');
+    //}
+//}
+
+function Validar () {
+    if(nome.value == ""){
+        alert('O Campo seu nome Precisa ser Preenchido');
+    }if ( email.value == "") {
+        alert('O Campo seu E-mail Precisa ser Preenchido');
+    }if ( message.value == "") {
+        alert('O Campo Mensagem Precisa ser Preenchido');
     } else {
         alert('Mensagem enviada com sucesso');
     }
